69dac6d711ace827e395b0b30d36829a574b3ae9
Srikanth Gurram <2gurram@gmail.com>
2016-11-25 23:07:49 +0530

    switched to gecko driver

--- src/main/java/util/DriverManager.java
+++ src/main/java/util/DriverManager.java
@@ -1,6 +1,8 @@
 package util;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 /**
  * Created by srikanth on 25/11/16.
  */
@@ -10,8 +12,10 @@ public class DriverManager {
     public static WebDriver driver;
     String baseUrl = "http://localhost:3000";
     public DriverManager(){
-        System.setProperty("webdriver.chrome.driver","/home/srikanth/Downloads/softwares/drivers/chromedriver");
-        driver = new ChromeDriver();
+//        System.setProperty("webdriver.chrome.driver","/home/srikanth/Downloads/softwares/drivers/chromedriver");
+//        driver = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver","/home/srikanth/Downloads/softwares/drivers/chromedriver");
+        driver = new FirefoxDriver();
         driver.get(baseUrl);
     }
 }
